e52041d76eb80bf2cf90ead1433dfa88d5417b02
huangl07 <290012645@qq.com>
2017-11-09 23:30:51 +0800

    biolfs

--- 20-download-packages.sh
+++ 20-download-packages.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-mkdir -P $HOME/biolfs
-wget http://mirrors.ustc.edu.cn/lfs/lfs-packages/lfs-packages-8.1-rc2.tar
+mkdir -p $HOME/biolfs
+wget http://mirrors.ustc.edu.cn/lfs/lfs-packages/lfs-packages-8.1-rc2.tar -P $HOME/biolfs
 pushd $HOME/biolfs
 popd
